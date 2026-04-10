d39c7b7b9da2793b1f14feac5f1c487421ac02ee
Siddhant-Latkar <siddhalatkar@gmail.com>
2026-04-10 15:50:54 +0530

    comparison in js

---
+++ comparison.js
@@ -0,0 +1,24 @@
+// ==============================
+// COMPARISON OPERATORS
+// ==============================
+
+console.log(10 == "10");  
+// true because only value is checked
+
+console.log(10 === "10"); 
+// false because data type is also checked
+
+console.log(20 != 10);    
+// true because values are different
+
+console.log(20 > 10);     
+// true because 20 is greater
+
+console.log(5 < 10);      
+// true because 5 is smaller
+
+console.log(10 >= 10);    
+// true because equal allowed
+
+console.log(5 <= 2);      
+// false because 5 is greater than 2
